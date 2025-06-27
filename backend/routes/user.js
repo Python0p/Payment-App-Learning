487cf40c2c6c63f0b5dde6d79f39f804208af6d8
Abhishek Gupta <abhishekgup232326@gmail.com>
2025-06-28 04:22:32 +0530

    added the middleware.js and updated the user.js for more requests

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const { User } = require("../db");
-const { JWTSECRET } = require("../config");
+const { JWT_SECRET } = require("../config");
 const zod = require("zod");
+const authMiddleware = require("../middleware");
 const router = express.Router();
 
 
@@ -12,6 +13,17 @@ const signupSchema = zod.object({
 	lastName: zod.string(),
 	password: zod.string(),
 
+})
+
+const signinSchema = zod.object({
+    username: zod.string().optional(),
+    password: zod.string().optional(),
+})
+
+const updateBodySchema = zod.object({
+    password: zod.string().optional(),
+    firstName: zod.string().optional(),
+    lastName: zod.string().optional(),
 })
 
 
@@ -33,14 +45,94 @@ router.post("/signup", async (req,res)=>{
 
     const dbUser = await User.create(body);
 
-    const token = jwt.sign({userId:dbUser._id},JWTSECRET);
+    const token = jwt.sign({userId:dbUser._id},JWT_SECRET);
 
     res.status(200).json({message: "User created",
         token: token
     });
 
+});
 
+router.post("/signin", async (req,res)=>{
+
+    const body = req.body;
+
+    const {success} = signinSchema.safeParse(body);
+
+    if(!success){
+        return res.status(411).json({message: "Error while logging in"});
+    }
+
+    const user = await User.findOne({
+        username:body.username,
+        password:body.password
+    });
+
+    if(!user){
+        return res.status(411).json({message: "Error while logging in"});
+    }
+
+    const token = jwt.sign({userId:user._id},JWT_SECRET);
+
+    return res.status(200).json({
+        token: token
+    });
     
+});
+
+router.put("/",authMiddleware, async (req,res)=>{
+    
+    const body = req.body;
+
+    const {success} = updateBodySchema.safeParse(body);
+
+    if(!success){
+        return res.status(411).json({message: "Error while logging in"});
+    }
+
+    await User.updateOne({
+        _id: req.userId
+    },req.body);
+
+    return res.status(200).json({
+        message: "Updated successfully"
+    });
+
+
+});
+
+
+
+router.get("/bulk", async (req, res) => {
+    const filter = req.query.filter || "";
+
+    // The above const filter is used beacause the url we get is like ?filter=harkirat so to get that we are 
+    // doing this
+
+    const users = await User.find({
+        $or: [{
+            firstName: {
+                "$regex": filter
+            }
+        }, {
+            lastName: {
+                "$regex": filter
+            }
+        }]
+    })
+
+    res.json({
+        user: users.map(user => ({
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            _id: user._id
+        }))
+    })
 })
+
+
+
+
 
 module.exports = router;
